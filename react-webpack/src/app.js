153fd0247c5cedc493fab0a0f554615a837866e4
DemetriusBS <dbsfox@outlook.com>
2020-12-08 14:56:01 -0300

    M2 - MRW#10 - Configurando a ferramenta de lint

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -3,9 +3,8 @@
 import React from 'react'
 
 const App = React.createClass({
-    render: function () {
-        return <h1>Aplicação 1</h1>
-    }
+  render: function () {
+    return <h1>Aplicação 1</h1>
+  }
 })
-
 export default App
--- react-webpack/src/index.js
+++ react-webpack/src/index.js
@@ -6,20 +6,19 @@ import { AppContainer } from 'react-hot-loader'
 import App from './app'
 
 const renderApp = (NextApp) => {
-    render(
-        <AppContainer>
-            <NextApp />
-        </AppContainer>,
-        document.querySelector('[data-js="app"]')
-    )
+  render(
+    <AppContainer>
+      <NextApp />
+    </AppContainer>,
+    document.querySelector('[data-js="app"]')
+  )
 }
 
 renderApp(App)
 
 if (module.hot) {
-    module.hot.accept('./app', () => {
-        const NextApp = require('./app').default
-        renderApp(NextApp)
-    })
+  module.hot.accept('./app', () => {
+    const NextApp = require('./app').default
+    renderApp(NextApp)
+  })
 }
-
