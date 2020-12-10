74a25df1f16689a18513b3e00705d6982751c2af
DemetriusBS <dbsfox@outlook.com>
2020-12-10 14:44:05 -0300

    M1 - #A08 - Atributos do HTML

--- learning-props/src/app.js
+++ learning-props/src/app.js
@@ -5,8 +5,10 @@ import Title from './title'
 
 const App = React.createClass({
   render: function () {
-    return <div>
+    return <div className='1'>
       <Title name='Demetrius' lastName='Batista Dos Santos' age={28} />
+      <label htmlFor='input'>Input</label>
+      <input type='text' id='input' />
     </div>
   }
 })
--- learning-props/src/title.js
+++ learning-props/src/title.js
@@ -5,7 +5,7 @@ import React from 'react'
 const Title = React.createClass({
   render: function () {
     return (
-    <h1> Olá {this.props.name + ' ' + this.props.lastName}! Você tem {this.props.age} anos</h1 >
+    <h1> Olá {this.props.name + ' ' + this.props.lastName}! Você tem {this.props.age} anos </h1 >
     )
   }
 })
