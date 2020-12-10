3bf809b3a0ba2c810e8f1cf640f8c2132f870b74
DemetriusBS <dbsfox@outlook.com>
2020-12-10 15:08:02 -0300

    M1 - #A09 - getDefaultProps

--- learning-props/src/app.js
+++ learning-props/src/app.js
@@ -6,9 +6,7 @@ import Title from './title'
 const App = React.createClass({
   render: function () {
     return <div className='1'>
-      <Title name='Demetrius' lastName='Batista Dos Santos' age={28} />
-      <label htmlFor='input'>Input</label>
-      <input type='text' id='input' />
+      <Title name='Demetrius ' />
     </div>
   }
 })
--- learning-props/src/title.js
+++ learning-props/src/title.js
@@ -3,9 +3,15 @@
 import React from 'react'
 
 const Title = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'Desconhecido(a)',
+      lastName: 'Sem sobrenome'
+    }
+  },
   render: function () {
     return (
-    <h1> Olá {this.props.name + ' ' + this.props.lastName}! Você tem {this.props.age} anos </h1 >
+      <h1> Olá {this.props.name + '' + this.props.lastName}! </h1 >
     )
   }
 })
