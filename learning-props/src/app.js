8606c6373605cc8ad13334694c69ba898c87c5b4
DemetriusBS <dbsfox@outlook.com>
2020-12-17 10:33:12 -0300

    M1 #A13 Conhecendo a prop "key"

--- learning-props/src/app.js
+++ learning-props/src/app.js
@@ -1,25 +1,18 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Title from './title'
+import Square from './square'
 
 class App extends Component {
   render() {
     return (
       <div className='container'>
-        <Title name='Demetrius' />
+        {['blue', 'red', 'green'].map((square) => (
+          <Square key={square} color={square} />
+        ))}
       </div>
     )
   }
 }
 
-/*
-const App = React.createClass({
-  render: function () {
-    return <div className='1'>
-      <Title />
-    </div>
-  }
-})
-*/
 export default App
---
+++ learning-props/src/square.js
@@ -0,0 +1,18 @@
+'use strict'
+
+import React from 'react'
+
+const Square = ({ color }) => (
+  <div style={{
+    backgroundColor: color,
+    height: '100px',
+    width: '100px'
+  }}>
+  </div>
+)
+
+Square.defaultProps = {
+  color: 'red'
+}
+
+export default Square
