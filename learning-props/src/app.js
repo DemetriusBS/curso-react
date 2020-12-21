4a4213a73a02e6344e812a380f5f2ba54a9e7a0f
DemetriusBS <dbsfox@outlook.com>
2020-12-21 09:47:12 -0300

    M1 #A15 - Eventos

--- learning-props/src/app.js
+++ learning-props/src/app.js
@@ -2,19 +2,14 @@
 
 import React, { Component } from 'react'
 import Square from './square'
-import Circle from './circle'
 
 class App extends Component {
   render() {
     return (
-      <div className='container'>
-        {['blue', 'red', 'blue'].map((square, index) => (
-          <Square key={index} color={square} />
-        ))}
-
-        {['green', 'black', 'green', 'black'].map((circle, index) => (
-          <Circle key={index} color={circle} />
-        ))}
+      <div className='container' onClick={(e) => {
+        alert('clicou')
+      }}>
+        <Square />
       </div>
     )
   }
