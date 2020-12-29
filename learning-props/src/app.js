8de7b6d3b8f0d534fdd65d52c83e05a6ec655c3f
DemetriusBS <dbsfox@outlook.com>
2020-12-29 10:46:24 -0300

    M1 #A22 Lyfecycle - Fuxo de montagem e desmontagem

--- learning-props/src/app.js
+++ learning-props/src/app.js
@@ -1,30 +1,35 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Button from './button'
-import Square from './square'
+import Timer from './timer'
 
 class App extends Component {
 
   constructor() {
+    console.log('constructor')
     super()
     this.state = {
-      color: 'green'
+      showTimer: true
     }
   }
 
+  componentWillMount() {
+    console.log('componentWillMount')
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+  }
+
   render() {
+    console.log('render')
     return (
       <div>
-        <Square color={this.state.color} />
+        {this.state.showTimer && <Timer />}
 
-        {['red', 'green', 'blue'].map((color) => (
-          <Button
-            key={color}
-            handleClick={() => this.setState({ color })}>
-            {color}
-          </Button>
-        ))}
+        <button onClick={() => {
+          this.setState({ showTimer: !this.state.showTimer })
+        }}>Show / hide timer</button>
       </div >
     )
   }
