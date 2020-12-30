0b7f2b8f403cd94358c8d586f77ce35fb90b085a
DemetriusBS <dbsfox@outlook.com>
2020-12-30 10:31:57 -0300

    M1 #A23 - LifeCycle - componentWillReceiveProps

--- learning-props/src/app.js
+++ learning-props/src/app.js
@@ -9,6 +9,7 @@ class App extends Component {
     console.log('constructor')
     super()
     this.state = {
+      time: 0,
       showTimer: true
     }
   }
@@ -23,13 +24,14 @@ class App extends Component {
 
   render() {
     console.log('render')
+
     return (
       <div>
-        {this.state.showTimer && <Timer />}
+        <Timer time={this.state.time} />
 
         <button onClick={() => {
-          this.setState({ showTimer: !this.state.showTimer })
-        }}>Show / hide timer</button>
+          this.setState({ time: this.state.time + 10 })
+        }}>Change props</button>
       </div >
     )
   }
