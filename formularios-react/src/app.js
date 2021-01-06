51b6a930c205cfe7a5a671563d085d94bc76e521
DemetriusBS <dbsfox@outlook.com>
2021-01-06 10:14:07 -0300

    M1 #A28 - Formularios-react

---
+++ formularios-react/src/app.js
@@ -0,0 +1,27 @@
+'use strict'
+
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      value: 'Valor inicial'
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input type='text' value={this.state.value} onChange={(e) => {
+            this.setState({
+              value: e.target.value
+            })
+          }} />
+        </form>
+      </div>
+    )
+  }
+}
+export default App
