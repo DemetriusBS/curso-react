7e7ed57f2f747f4ec50ee03f524548b53d8da079
DemetriusBS <dbsfox@outlook.com>
2021-01-11 14:46:06 -0300

    M1 #A30 Formulários (Select e Option)

--- formularios-react/src/app.js
+++ formularios-react/src/app.js
@@ -3,36 +3,26 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+
   constructor() {
     super()
     this.state = {
-      value: 'Valor inicial',
-      checked: false
+      value: '2'
     }
   }
 
   render() {
     return (
       <form>
-        <input type='text' value={this.state.value} onChange={(e) => {
+        <select value={this.state.value} onChange={(e) => {
           this.setState({
             value: e.target.value
           })
-        }} />
-
-        <label>
-          <input type='checkbox'
-            value='my-checkbox'
-            checked={this.state.checked}
-            onChange={(e)=> 
-              this.setState({ checked: !this.state.checked}) }
-               />
-            Checkbox
-          </label>
-
-          <input type='radio' name='rd' defaultChecked /> Radio 1
-          <input type='radio' name='rd' value='2' /> Radio 2
-
+        }}>
+          <option value='1'>Opção 1</option>
+          <option value='2'>Opção 2</option>
+          <option value='3'>Opção 3</option>
+        </select>
       </form>
     )
   }
