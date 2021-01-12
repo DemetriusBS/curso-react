1771bfce866588dfc45db81d56275848d901386f
DemetriusBS <dbsfox@outlook.com>
2021-01-12 08:48:59 -0300

    Projeto Gatry Clone - React

--- pratica-react/src/app.js
+++ pratica-react/src/app.js
@@ -1,29 +1,12 @@
 import React, { Component } from 'react'
 
 class Teste extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showImage: false,
-    };
-  }
-
-  mudaEstado = () => {
-    this.setState({
-      showImage: !this.state.showImage,
-    });
-  }
+  
 
   render() {
     return (
       <div className='App'>
-        <button type='button' onClick={this.mudaEstado}>
-          {this.state.showImage ? 'Esconder' : 'Mostrar'}
-        </button>
-
-        {this.state.showImage && (
-          <img src='./src/image/react-logo.png' className='img' />
-        )}
+        
       </div>
     );
   }
