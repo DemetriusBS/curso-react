8e835d5deae4f12603edc43ea4e0f1988f3287eb
DemetriusBS <dbsfox@outlook.com>
2021-02-18 10:39:57 -0300

    TDD

--- learning-tests/test/sum-test.js
+++ learning-tests/test/sum-test.js
@@ -1,4 +1,15 @@
 const sum = require('./sum')
 
-console.log(sum(1, 2) === 3)
-console.log(sum (1, 1) === 5)
+console.assert(typeof sum === 'function',
+    'sum deve ser uma função'
+)
+
+console.assert(
+    sum(1, 2) === 3,
+    'Deve retornar 3'
+)
+
+console.assert(
+    sum(2, 3) === 5,
+    'Deve retornar 5'
+)
--- learning-tests/test/sum.js
+++ learning-tests/test/sum.js
@@ -1,3 +1,4 @@
+
 function sum(x, y) {
     return x + y
 }
