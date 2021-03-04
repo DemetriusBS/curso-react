0d6f68c261acf1212180d7a1e54f9bc4df6b9e5e
DemetriusBS <dbsfox@outlook.com>
2021-03-04 14:30:13 -0300

    M2 #A08 Jest na prática - Watch interativo

---
+++ learning-tests/test/title-test.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const React = require('react')
+const ReactDOMServer = require('react-dom/server')
+const Title = require('./title')
+
+const TitleComponent = ReactDOMServer.renderToStaticMarkup(
+    React.createElement(Title)
+)
+
+console.log(TitleComponent)
