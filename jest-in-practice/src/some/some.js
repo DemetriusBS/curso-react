a119a93f6d3b4fc8aa95ae6604538779681730e7
DemetriusBS <dbsfox@outlook.com>
2021-04-07 14:43:26 -0300

    M2 #A18 - TDD - Some

---
+++ jest-in-practice/src/some/some.js
@@ -0,0 +1,9 @@
+const some = (arr, func) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i], i, arr)) {
+            return true
+        }
+    }
+    return false
+}
+export default some
