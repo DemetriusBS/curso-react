a3fdb73fa344b44eace30c8f3d24dd335530d4ba
DemetriusBS <dbsfox@outlook.com>
2021-04-12 13:49:26 -0300

    M2 #A19 - TDD - Some Recursive

---
+++ jest-in-practice/src/some/some-recursive.js
@@ -0,0 +1,12 @@
+const some = (arr, func) => {
+    return (function someInternal(arrayInternal, counter) {
+        const [head, ...tail] = arrayInternal
+
+        return arrayInternal.length === 0
+            ? false
+            : func(head, counter, arr)
+                ? true
+                : someInternal(tail, counter + 1)
+    })(arr, 0)
+}
+export default some
